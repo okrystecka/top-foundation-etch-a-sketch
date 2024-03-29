09f33bc588abb42bef7276b777a8ca2f213a8bac
Aleksandra Krystecka <okrystecka@gmail.com>
2024-03-29 21:20:41 +0100

    Dynamic drawing size

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 const gridContainer = document.querySelector('#container');
 const resetButton = document.querySelector('#reset');
+const sizeButton = document.querySelector('#sizeButton');
+const sizeInput = document.querySelector('#sizeInput');
+
+const CONTAINER_SIZE = 480;
+let gridSize = 16;
+
+function handleHover(e) {
+    e.target.classList.add('hover');
+}
+
+function handlePaint(e) {
+    e.target.classList.add('painted');
+}
+
+function handleReset() {
+    const nodeList = document.querySelectorAll('.square');
+    for(const el of nodeList) {
+        el.remove();
+    }
+    createGrid();
+}
+
+function handleSetSize(e) {
+    const preferredSize = sizeInput.value;
+    if(preferredSize > 100) {
+        console.log("Prefered size is too big!");
+        return;
+    }
+    gridSize = preferredSize;
+    handleReset();
+    sizeInput.value = '';
+    sizeInput.focus();
+}
+
+function calculateSquareSize() {
+    return squareSize = CONTAINER_SIZE / gridSize;
+}
 
 function createGrid() {
-    for (let i = 0; i < (16*16); i++) {
+    for (let i = 0; i < (gridSize*gridSize); i++) {
         gridContainer.appendChild(createSquare());
     }
 }
@@ -10,21 +47,13 @@ function createGrid() {
 function createSquare() {
     const square = document.createElement('div');
     square.classList.add('square')
+    square.style.width = calculateSquareSize() + 'px';
+    square.style.height = calculateSquareSize() + 'px';
     square.addEventListener('mouseover', handleHover);
+    square.addEventListener('click', handlePaint);
     return square;
 }
 
-function handleHover(e) {
-    e.target.classList.add('hover');
-}
-
-function handleReset(e) {
-    console.log('Reset');
-    const nodeList = document.querySelectorAll('.square');
-    for(const el of nodeList) {
-        el.classList.remove('hover');
-    }
-}
-
+sizeButton.addEventListener('click', handleSetSize);
 resetButton.addEventListener('click', handleReset);
-createGrid();
+handleReset();
